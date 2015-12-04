93c2f9a6f87471e6a17e06480fbfac00f5f42be6
Ryan Dwyer <ryankdwyer@gmail.com>
2015-12-04 09:59:29 -0500

    Added volume slider

--- app/components/player/player.controller.js
+++ app/components/player/player.controller.js
@@ -4,6 +4,7 @@ app.controller('PlayerCtrl', function ($scope, PlayerService, $rootScope) {
     $scope.totalTime = null;
     $scope.duration = 0;
     var progressBar = $id('songDuration');
+    var volumeBar = $id('volume');
 
     function $id(id) {
         return document.getElementById(id);
@@ -19,4 +20,12 @@ app.controller('PlayerCtrl', function ($scope, PlayerService, $rootScope) {
     	$scope.$apply($scope.duration = elapsed);
         progressBar.value = elapsed;
     });
+
+    $scope.changeVolume = function (value) {
+        $scope.Player.player.volume = value;
+    }
+
+    $scope.seekTo = function (value) {
+        $scope.Player.player.device.seek(value/1000);
+    }
 });
