5a556e2cf90cac4caa93d8ecf84b075d3603e9c3
Ryan Dwyer <ryankdwyer@gmail.com>
2015-12-08 09:17:50 -0500

    hold on visualizer - too computationally expensive

--- app/components/visualizer/visualizer.controller.js
+++ app/components/visualizer/visualizer.controller.js
@@ -1,19 +1,32 @@
-app.controller('VisualizerCtrl', function ($scope, PlayerService, $rootScope, $interval, VisualizerService) {
+app.controller('VisualizerCtrl', function ($scope, PlayerService, $rootScope, $interval) {
     //$rootScope.$on('songStarted', function () {
-    //    VisualizerService.freqData = new Array(Math.floor(PlayerService.player.asset.decoder.blockSize / 100));
-    //    VisualizerService.svg = VisualizerService.createSvg('#viz', VisualizerService.svgHeight, VisualizerService.svgWidth);
-    //
-    //    VisualizerService.svg.selectAll('rect')
-    //        .data(VisualizerService.freqData)
-    //        .enter()
-    //        .append('rect')
-    //        .attr('x', function (d, i) {
-    //            return i * (VisualizerService.svgWidth / VisualizerService.freqData.length);
-    //        })
-    //        .attr('width', VisualizerService.svgWidth / VisualizerService.freqData.length - VisualizerService.barPadding);
-    //    console.log(VisualizerService.svg);
+    //    //var fft = new FFT.complex(1, true);
+    //    //console.log(fft, FFT);
+    ////    VisualizerService.freqData = new Array(Math.floor(PlayerService.player.asset.decoder.blockSize / 100));
+    ////    VisualizerService.svg = VisualizerService.createSvg('#viz', VisualizerService.svgHeight, VisualizerService.svgWidth);
+    ////
+    ////    VisualizerService.svg.selectAll('rect')
+    ////        .data(VisualizerService.freqData)
+    ////        .enter()
+    ////        .append('rect')
+    ////        .attr('x', function (d, i) {
+    ////            return i * (VisualizerService.svgWidth / VisualizerService.freqData.length);
+    ////        })
+    ////        .attr('width', VisualizerService.svgWidth / VisualizerService.freqData.length - VisualizerService.barPadding);
+    ////    console.log(VisualizerService.svg);
+    //    var counter = 0;
     //    PlayerService.player.asset.decoder.on('data', function (data) {
-    //        VisualizerService.renderChart(data, VisualizerService.svg);
+    //        if (counter % 10 === 0) {
+    //            var result = fft(data);
+    //            console.log(result);
+    //            var freq = fftUtil.fftFreq(result.slice(0, result.length / 2), 44100);
+    //            var magnitudes = fftUtil.fftMag(result.slice(0, result.length / 2));
+    //            //var both = freq.map(function (f, ix) {
+    //            //    return {frequency: f, magnitude: magnitudes[ix]};
+    //            //});
+    //            //console.log(both);
+    //        }
+    //        counter++;
     //    });
     //})
 });
