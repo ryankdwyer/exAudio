14e5b94a9b79f7c8d794bd9f4ad4e4abdef1cb8c
Ryan Dwyer <ryan@Ryans-MacBook-Pro.local>
2016-02-08 18:23:38 -0500

    fied snap header

--- app/shared/setClassAtTop/setClassAtTop.js
+++ app/shared/setClassAtTop/setClassAtTop.js
@@ -3,17 +3,21 @@ app.directive('setClassAtTop', ($window, $document) => {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-            var topClass = "fixed top-0 block",
-                offsetTop = 68;
+            var topClass = "fixed top-0 left-0 right-0",
+                hideElementClass = 'hide-element',
+                offsetTop = 69;
             var header = angular.element(document.querySelector('#libraryHeader'));
+            var spacer = angular.element(document.querySelector('#spacer'));
             $win.on('scroll', function (e) {
                 if ($win[0].pageYOffset >= offsetTop) {
                     console.log('condition satisfied');
+                    spacer.removeClass(hideElementClass);
                     element.removeClass('clearfix');
                     element.addClass(topClass);
                 } else {
                     element.removeClass(topClass);
                     element.addClass('clearfix');
+                    spacer.addClass(hideElementClass);
                 }
             });
         }
