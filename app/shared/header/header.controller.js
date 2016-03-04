0630e067c519340338ca1a17d47ebc81f23de77b
Ryan Dwyer <ryan@Ryans-MacBook-Pro.local>
2016-03-04 17:00:06 -0500

    refactoring playerservice

--- app/shared/header/header.controller.js
+++ app/shared/header/header.controller.js
@@ -1,8 +1,12 @@
-app.controller('HeaderCtrl', ($scope, PlayerService, Storage) => {
-    $scope.openAddSongs = () => {
-        ipc.send('open-add-songs');
-    };
-    $scope.openFindSimilar =  () => {
-        ipc.send('find-similar', PlayerService.player);
-    };
+app.controller('HeaderCtrl', ($scope, PlayerService, Storage, echoNestFactory) => {
+  $scope.openAddSongs = () => {
+    ipc.send('open-add-songs');
+  };
+  $scope.findSimilarSongs = () => {
+    var metadata = PlayerService.getMetadata(PlayerService.player);
+    echoNestFactory.getSimilarArtist(metadata)
+      .then(function(data) {
+        console.log(data);
+      })
+  }
 });
